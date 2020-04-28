42415f8eeadc568180ba9999db53577519c0ac50
Eldh97 <eldh97@outlook.com>
2020-04-28 16:01:15 +0300

    Initial Commit

---
+++ pitchTests/pitchTests.swift
@@ -0,0 +1,34 @@
+//
+//  pitchTests.swift
+//  pitchTests
+//
+//  Created by Abdulrahman Alotaibi on 05/09/1441 AH.
+//  Copyright © 1441 Abdulrahman Alotaibi. All rights reserved.
+//
+
+import XCTest
+@testable import pitch
+
+class pitchTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
